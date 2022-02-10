3d548052404e9854730eeea5db3ba7d68e400240
Artem <arteam_101@mail.ru>
2022-02-10 21:26:30 +0700

    Думал-думал, да что-то и придумал

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,11 +1,37 @@
 // See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Linq;
+using Avalonia.OpenGL;
+
+public class HW1
+{
+    public static long QueueTime(int[] customers, int n)
+    {
+        int theTime = 0;
+        while (customers.Length > 0)
+        {
+            int busyCashiers = n > customers.Length ? customers.Length : n;
+            for (int i = 0; i < busyCashiers; i++)
+            {
+                customers[i]--;
+            }
+            customers = Array.FindAll(customers, x => x != 0);
+
+            theTime++;
+        }
+        return theTime;
+    }
+}
 
 class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, world!");
-    }
+        Console.WriteLine(HW1.QueueTime( new int [] {5, 3, 4 }, 1) == 12);
+        Console.WriteLine(HW1.QueueTime( new int [] {10, 2, 3, 3}, 2) == 10);
+        Console.WriteLine(HW1.QueueTime( new int [] {2, 3, 10 }, 2) == 12);
+
+
+}
 }
