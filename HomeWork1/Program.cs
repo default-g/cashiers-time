2a2b1b3888c66d30aa71691540d3d20e2030be1e
Obuhov Artem <64357734+default-g@users.noreply.github.com>
2022-02-10 20:25:57 +0700

    Initial commit

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,11 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
